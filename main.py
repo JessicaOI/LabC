ec56ce8b1985c6af0a4f4cca79aacdd0b0698e3a
JessicaOI <70552512+JessicaOI@users.noreply.github.com>
2023-04-06 19:16:16 -0600

    codigo antes del review

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
-from classAutomata import Automata
 from funciones import parse_yalex, generate_automata, automata_to_state_diagram
 
 def main():
-    # Leer el archivo de entrada YALex
     input_file = "EjemploBasico.txt"
-    
-    # Obtener las definiciones regulares del archivo
     regular_definitions = parse_yalex(input_file)
-    
-    # Construir el autómata a partir de las definiciones regulares
     automata = generate_automata(regular_definitions)
-    
-    # Obtener el diagrama de transición de estados
-    state_diagram = automata_to_state_diagram(automata)
-    
-    # Guardar o imprimir el diagrama de transición de estados
-    print(state_diagram)
+    state_diagrams = automata_to_state_diagram(automata)
+
+    for token, state_diagram in state_diagrams.items():
+        print(f"Token: {token}")
+        print(f"State diagram: {state_diagram}")
+        print()
 
 if __name__ == "__main__":
     main()
